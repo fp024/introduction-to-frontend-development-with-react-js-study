fad5a352c6c7656d30c74f5f7cf002107e13c91b
fp024 <aopenasus@naver.com>
2025-09-04 14:26:35 +0900

    이벤트 객체와 매개변수 ~ p127

--- chap03/vite-react-chap03/src/App.tsx
+++ chap03/vite-react-chap03/src/App.tsx
@@ -1,6 +1,10 @@
 export default function App() {
-  const clickHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const clickHandler = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>, //
+    msg: string
+  ) => {
     console.log(e);
+    alert(msg);
   };
-  return <button onClick={clickHandler}>click</button>;
+  return <button onClick={(e) => clickHandler(e, 'Hello')}>click</button>;
 }
