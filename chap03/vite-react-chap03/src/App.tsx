18d0491aba6c44aee7b2971475318fb038c2e8a1
fp024 <aopenasus@naver.com>
2025-09-04 14:48:36 +0900

    버블링 ~ p129
    자식 -> 부모 이벤트 전파

--- chap03/vite-react-chap03/src/App.tsx
+++ chap03/vite-react-chap03/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   };
 
   return (
-    <div onClickCapture={handleCapture} style={{ padding: '50px', backgroundColor: '#f0f0f0' }}>
+    <div onClick={handleCapture} style={{ padding: '50px', backgroundColor: '#f0f0f0' }}>
       Parent{' '}
       <button onClick={handleBubble} style={{ marginTop: '20px' }}>
         Click Me
