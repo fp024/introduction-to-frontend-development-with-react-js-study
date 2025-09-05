41953c5f7c46ec8c125a2eed122b79b442d7c440
fp024 <aopenasus@naver.com>
2025-09-05 12:27:37 +0900

    4.1 컴포넌트의 상태란 ~ p142
    name에 대한 상태가 관리되지 않아
    화면에 이름이 바뀌진 않는다.

--- chap04/vite-react-chap04/src/App.tsx
+++ chap04/vite-react-chap04/src/App.tsx
@@ -1,10 +1,15 @@
 export default function App() {
-  const name = '철수';
+  let name = '철수';
   const age = 20;
+  const nameChange = () => {
+    name = '영희';
+    console.log(name);
+  };
   return (
     <div>
       <p>{name}</p>
       <p>{age}</p>
+      <button onClick={nameChange}>Value Change</button>
     </div>
   );
 }
