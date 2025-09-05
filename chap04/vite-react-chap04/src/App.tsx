9b5e817d2e60c5c2805d0f881e5fa076f12f5b1d
fp024 <aopenasus@naver.com>
2025-09-05 13:33:59 +0900

    상태 변경함수에 콜백함수 형태 사용 ~ p155
    이제 3씩 업데이트가 잘된다.

--- chap04/vite-react-chap04/src/App.tsx
+++ chap04/vite-react-chap04/src/App.tsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 export default function App() {
   const [count, setCount] = useState(0);
   const clickHandler = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
   };
 
   return (
