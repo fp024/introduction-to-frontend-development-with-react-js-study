3fc0e6488c64c66575cad89c13d1f0596c3d624d
fp024 <aopenasus@naver.com>
2026-01-22 13:37:14 +0900

    5.2.2 emotion - 동적 스타일

--- chap05/vite-react-chap05-2-2/src/App.tsx
+++ chap05/vite-react-chap05-2-2/src/App.tsx
@@ -1,19 +1,23 @@
 import { css } from '@emotion/css';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isActive, setIsActive] = useState(true);
+
   return (
     <button
       type="button"
       className={css`
         cursor: pointer;
-        background: transparent;
+        background: ${isActive ? '#bf4f74' : 'transparent'};
         border-radius: 3px;
         border: 2px solid #bf4f74;
-        color: #bf4f74;
+        color: ${isActive ? 'white' : '#bf4f74'};
         margin: 0 1em;
         padding: 0.25em 1em;
       `}
+      onClick={() => setIsActive(!isActive)}
     >
       Click Me
     </button>
