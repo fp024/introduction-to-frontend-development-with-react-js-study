157f9d53ec663d8dd2c90e32cced63181cf46d58
fp024 <aopenasus@naver.com>
2026-01-22 22:54:02 +0900

    5.3.1 - tailwindcss 기본 사용법

--- chap05/vite-react-chap05-3/src/App.tsx
+++ chap05/vite-react-chap05-3/src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css';
 
 function App() {
-  return <div>Hello, React!</div>;
+  return (
+    <button
+      type="button"
+      className="
+       bg-transparent
+       rounded-[3px]
+       border-2 
+      border-[#bf4f74]
+      text-[#bf4f74]
+       m-[0.1em]
+       py-[0.25em]
+       px-[1em]
+       cursor-pointer"
+    >
+      Click Me
+    </button>
+  );
 }
 
 export default App;
