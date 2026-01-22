424a87e1db79f502802a724d16c9076be5559313
fp024 <aopenasus@naver.com>
2026-01-22 14:36:03 +0900

    5.2.3 vanilla-extract - 동적 스타일

--- chap05/vite-react-chap05-2-3/src/App.tsx
+++ chap05/vite-react-chap05-2-3/src/App.tsx
@@ -1,9 +1,18 @@
-import { container, button } from './App.css.ts';
+import { useState } from 'react';
+import { container, button, active } from './App.css.ts';
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActive = () => {
+    setIsActive((prev) => !prev);
+  };
+
   return (
     <div className={container}>
-      <button className={button}>Click me</button>
+      <button type="button" className={`${button} ${isActive && active}`} onClick={toggleActive}>
+        Click me
+      </button>
     </div>
   );
 }
