c994457b3d52935ec52336b5ba3dc4d9eb2e4564
fp024 <aopenasus@naver.com>
2026-01-22 09:31:15 +0900

    5.1.1 인라인 스타일

---
+++ chap05/vite-react-chap05/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const styles = {
+    backgroundColor: 'blue',
+    color: 'white',
+    fontSize: '16px',
+    padding: '10px',
+  };
+  return <h1 style={styles}>Inline Style</h1>;
+}
+
+export default App;
