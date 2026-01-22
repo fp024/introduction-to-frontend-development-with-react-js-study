d35f5976da81b1ddb46bdedc3883a7aab0afb89f
fp024 <aopenasus@naver.com>
2026-01-22 13:16:26 +0900

    5.2.1. styled-components - 동적 스타일

--- chap05/vite-react-chap05-2/src/App.tsx
+++ chap05/vite-react-chap05-2/src/App.tsx
@@ -1,17 +1,34 @@
 import styled from 'styled-components';
 import './App.css';
+import { useState } from 'react';
 
-const Button = styled.button`
+const Button = styled.button<{ $primary?: boolean }>`
   background: transparent;
   border-radius: 3px;
   border: 2px solid #bf4f74;
   color: #bf4f74;
   margin: 0 1em;
   padding: 0.25em 1em;
+  ${(props) =>
+    props.$primary &&
+    `
+    background: #bf4f74;
+    color: white;
+  `}
 `;
 
 function App() {
-  return <Button>Click Me</Button>;
+  const [primary, setPrimary] = useState(false);
+
+  const togglePrimary = () => {
+    setPrimary(!primary);
+  };
+
+  return (
+    <Button $primary={primary} onClick={togglePrimary}>
+      Click Me
+    </Button>
+  );
 }
 
 export default App;
