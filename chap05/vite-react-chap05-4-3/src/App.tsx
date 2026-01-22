40e626e72d7088667bf35fce592fd244a7953d4e
fp024 <aopenasus@naver.com>
2026-01-23 03:30:39 +0900

    5.4.3 src 폴더에서 이미지 렌더링하기
    img태그, css속성으로 랜더링하기 둘다 해보았다.

---
+++ chap05/vite-react-chap05-4-3/src/App.tsx
@@ -0,0 +1,12 @@
+import ocean from './assets/images/ocean.jpg';
+
+function App() {
+  return (
+    <div>
+      <img src={ocean} alt="ocean" />
+      <div className="bg"></div>
+    </div>
+  );
+}
+
+export default App;
