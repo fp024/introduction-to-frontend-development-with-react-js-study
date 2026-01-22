28e64836414e9f562e1f46ff8678826b465a2224
fp024 <aopenasus@naver.com>
2026-01-22 14:17:13 +0900

    5.2.3 vanilla-extract - 기본 사용법

---
+++ chap05/vite-react-chap05-2-3/README.md
@@ -0,0 +1,7 @@
+# 5.2.3 vanilla-extract
+
+* vanilla-extract 설치
+  ```sh
+  pnpm add @vanilla-extract/css
+  pnpm add -D @vanilla-extract/vite-plugin
+  ```
---
+++ chap05/vite-react-chap05-2-3/src/App.tsx
@@ -0,0 +1,11 @@
+import { container, button } from './App.css.ts';
+
+function App() {
+  return (
+    <div className={container}>
+      <button className={button}>Click me</button>
+    </div>
+  );
+}
+
+export default App;
